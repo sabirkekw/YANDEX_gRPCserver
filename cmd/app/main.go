82f72ba29256c930dee8b084483d760f0c9bb598
sabirkekw <smagaramov6@gmail.com>
2026-02-20 17:33:00 +0300

    uber zap

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,13 +4,19 @@ import (
 	"fmt"
 
 	"github.com/sabirkekw/YANDEX_gRPCserver/internal/cfg"
+	"github.com/sabirkekw/YANDEX_gRPCserver/internal/domain/logger"
 )
 
 func main() {
+	logger.InitLogger()
+	defer logger.Log.Sync()
+	logger.Log.Infow("Logger initialized")
+
 	config := cfg.MustLoad()
-	fmt.Print(config)
-	// todo: init logger
+	logger.Log.Infow("Config loaded", "config", fmt.Sprintf("%+v", config))
+
 	// todo: init database
-	// todo: start server
+	// todo: init app
+	// todo: start server (app.Run())
 	// todo: handle graceful shutdown
 }
--- go.mod
+++ go.mod
@@ -11,6 +11,8 @@ require (
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.27.1 // indirect
 	golang.org/x/net v0.48.0 // indirect
 	golang.org/x/sys v0.39.0 // indirect
 	golang.org/x/text v0.32.0 // indirect
